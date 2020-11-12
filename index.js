f774e5cdc1380bdbac81cc1da8329f7f013d8d45
UserAx <su.userax@gmail.com>
2020-11-12 12:49:08 +0545

    fourth commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config({path: './config/dev.env'});
 const express = require('express');
 const app = express();
-const cors = require('cors');
-app.use(cors());
+//const cors = require('cors');
+//app.use(cors());
 const userrouter = require('./src/routers/userrouter');
 const messagerouter = require('./src/routers/messagerouter');
 const notificationrouter = require('./src/routers/notificationrouter');
