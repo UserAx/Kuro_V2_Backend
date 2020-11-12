a32f8e5dd318a234a9f7697852cad73af2a5b458
UserAx <su.userax@gmail.com>
2020-11-12 13:33:17 +0545

    fifth commit

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ require('dotenv').config({path: './config/dev.env'});
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const whitelist = [process.env.CORS_FRONTEND_TEST, process.env.CORS_FRONTEND_PROD];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-        //doesn't equal -1. (if the link does exist in whitelist array, we will get 0 and above value.)
-      callback(null, true);//in that case, return true and allow.
-    } else {
-      callback(new Error('Not allowed by CORS'));//else throw an error.
-    }
-  }
+// const whitelist = [process.env.CORS_FRONTEND_TEST, process.env.CORS_FRONTEND_PROD];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//         //doesn't equal -1. (if the link does exist in whitelist array, we will get 0 and above value.)
+//       callback(null, true);//in that case, return true and allow.
+//     } else {
+//       callback(new Error('Not allowed by CORS'));//else throw an error.
+//     }
+//   }
+// }
+const corsOption = {
+    origin: process.env.CORS_FRONTEND_TEST,
+    optionsSuccessStatus: 200
 }
-app.use(cors(corsOptions));
+app.use(cors(corsOption));
 const userrouter = require('./src/routers/userrouter');
 const messagerouter = require('./src/routers/messagerouter');
 const notificationrouter = require('./src/routers/notificationrouter');
