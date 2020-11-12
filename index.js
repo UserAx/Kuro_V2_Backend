581772bc378ca1d55b97651109d4e2eecc818711
UserAx <su.userax@gmail.com>
2020-11-12 13:15:37 +0545

    fifth commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require('dotenv').config({path: './config/dev.env'});
 const express = require('express');
 const app = express();
-//const cors = require('cors');
-//app.use(cors());
+const cors = require('cors');
+const whitelist = [process.env.CORS_FRONTEND_TEST, process.env.CORS_FRONTEND_PROD];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+        //doesn't equal -1. (if the link does exist in whitelist array, we will get 0 and above value.)
+      callback(null, true);//in that case, return true and allow.
+    } else {
+      callback(new Error('Not allowed by CORS'));//else throw an error.
+    }
+  }
+}
+app.use(cors(corsOptions));
 const userrouter = require('./src/routers/userrouter');
 const messagerouter = require('./src/routers/messagerouter');
 const notificationrouter = require('./src/routers/notificationrouter');
